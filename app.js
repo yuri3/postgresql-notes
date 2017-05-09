9b62c665b61a9ea0d922d76365a50eebc8ded29a
Yury <yury13t@mail.com>
2017-05-09 06:21:09 +0300

    update code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Last-Modified', (new Date()).toUTCString());
+  res.header('Cache-Control', 'no-cache, no-store');
   next();
 });
 
