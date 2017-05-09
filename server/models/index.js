5a8a8179783cb8b0c90ea7b12fb85c94fe2d38d3
Yury <yury13t@mail.com>
2017-05-09 05:16:59 +0300

    update code

--- server/models/index.js
+++ server/models/index.js
@@ -11,6 +11,9 @@ if(process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
+    port: match[4],
+    host: match[3],
+    logging: true,
   });
 } else if(config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
