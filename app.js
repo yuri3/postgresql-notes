b9757cde323d3eb0d5ecd3216f3530e5c520ca6c
Yury <yury13t@mail.com>
2017-05-09 06:13:16 +0300

    update code

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Last-Modified', (new Date()).toUTCString());
+  res.header('Last-Modified', (new Date()).toUTCString());
   next();
 });
+
+app.disable('etag');
 
 app.get('/', (req, res) => res.send({
   message: 'Welcome.',
