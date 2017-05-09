0574934656035de3c4f276840ae5f643adbcabd4
Yury <yury13t@mail.com>
2017-05-09 04:03:56 +0300

    update code

--- server/models/index.js
+++ server/models/index.js
@@ -8,13 +8,7 @@ const db = {};
 
 let sequelize;
 if(process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    port: match[4],
-    host: match[3],
-    logging: true,
-  });
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else if(config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
@@ -38,9 +32,7 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-global.db = {
-  Sequelize: Sequelize,
-  sequelize: sequelize,
-};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports = global.db;
+module.exports = db;
